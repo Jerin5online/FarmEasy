cc12ae39322dd3e69600983eed89e23c2bff4190
Jerin5online <jerin956@gmail.com>
2024-03-18 14:05:57 +0530

    change

--- src/components/profilepage/ProfilePage.jsx
+++ src/components/profilepage/ProfilePage.jsx
@@ -1,31 +1,100 @@
 import React, { useEffect, useState } from 'react'
 import MyHeader from '../myHeader/MyHeader'
 import './ProfilePage.css'
-import { addnewsAPI, adduserAPI } from '../../Services/AllAPI'
+import { EdituserprofileAPI, addnewsAPI, adduserAPI } from '../../Services/AllAPI'
 import Addproduct from '../Addproducts/Addproduct'
 
+import Swal from 'sweetalert2'
+// 
 const ProfilePage = () => {
+  const[userprofile,setUserprofile] = useState({
+          user_type: "",
+          username: "",
+          email: "",
+          phone: "",
+          address: "",
+          location: "",
+  })
 
- const [userprodetails,setUserprodetails]= useState([])
+  const[preview,setPreview]=useState("")
+  const [isUpdate,setIsUpdate] = useState(false)
 
-const getUserDetails = async()=>{
-  if (sessionStorage.getItem("token")) {
-    const token = sessionStorage.getItem("token")
-    const reqHeader = {
 
-      "Content-Type": "application/json",
-      "Authorization": `Token ${token}`
-    
-  }
-  const result = await adduserAPI(reqHeader)
-      console.log(result.data.data);
-      setUserprodetails(result.data.data)
+useEffect(()=>{
+const user1 = JSON.parse(sessionStorage.getItem("user"))
+
+setUserprofile({...userprofile,username:user1.username,email:user1.email,address:user1.address,location:user1.location,phone:user1.phone,user_type:user1.user_type})
+
+},[isUpdate])
+
+const handleProfileUpdate = async()=>{
+  const{user_type,username,email,phone,address,location} = userprofile
+  if(!user_type || !username || !email ||  !phone || !address || !location){
+    Swal.fire({
+      title: "Incomplete Form !",
+      text: "Please fill in the form",
+      icon: "error",
+    });
+  
+
+}
+else{
+  const reqBody = new FormData()
+  reqBody.append("user_type",user_type)
+  reqBody.append("username",username)
+  reqBody.append("email",email)
+  reqBody.append("phone",phone)
+  reqBody.append("address",address)
+  reqBody.append("location",location)
+
+const token = sessionStorage.getItem("token")
+if(preview){
+  const reqHeader = {
+    "Authorization" :`Token ${token}`
+   }
+   const result = await EdituserprofileAPI(reqBody,reqHeader)
+   console.log(result);
+   if(result.status === 200){
+    Swal.fire({
+      title: "Profile Update Successfully",
+      text: "Please fill in the form",
+      icon: "success",
+    });
+    sessionStorage.setItem("user",JSON.stringify(result.data))
+    setIsUpdate(true)
+   
+   }
+   else{
+console.log(result.response.data.user);
+
+   }
+}
+else{
+  const reqHeader = {
+    "Content-Type": "application/json",
+    "Authorization": `Token ${token}`,
+  };
+  const result = await EdituserprofileAPI(reqBody,reqHeader)
+   console.log(result.data.user);
+   if(result.status ===200)
+   {
+    Swal.fire({
+      title: "Profile Update Successfully",
+      text: "Please fill in the form",
+      icon: "success",
+    });
+    sessionStorage.setItem("user",JSON.stringify(result.data))
+    setIsUpdate(true)
+
+   }
+   else{
+console.log(result.response.data.user);
+
+   }
+}
+
 }
 }
-useEffect(() => {
-  getUserDetails()
-}, [])
-
   return (
    <>
    <MyHeader/>
@@ -51,27 +120,30 @@ useEffect(() => {
             <div className="row mt-2">
               <div className="col-md-6">
                 <label className="labels">User Name</label>
-                <input type="text" className="form-control" placeholder=""/>
+                <input  value={userprofile.username} onChange={(e) => setUserprofile({ ...userprofile, username: e.target.value })} type="text" className="form-control" placeholder=""/>
               </div>
               <div className="col-md-6">
                 <label className="labels">Email</label>
-                <input type="text" className="form-control"   />
+                <input  value={userprofile.email} onChange={(e) => setUserprofile({ ...userprofile, email: e.target.value })}  type="text" className="form-control"   />
               </div>
              <div className="col-md-6">
                 <label className="labels">Phone</label>
-                <input type="text" className="form-control"   />
-              </div> <div className="col-md-6">
+                <input value={userprofile.phone} onChange={(e) => setUserprofile({ ...userprofile, phone: e.target.value })}  type="text" className="form-control"   />
+              </div>
+               <div className="col-md-6">
                 <label className="labels">Address</label>
-                <input type="text" className="form-control"   />
-              </div> <div className="col-md-6">
+                <input value={userprofile.address} onChange={(e) => setUserprofile({ ...userprofile, address: e.target.value })}  type="text" className="form-control"   />
+              </div> 
+              
+              <div className="col-md-6">
                 <label className="labels">Location</label>
-                <input type="text" className="form-control"   />
+                <input value={userprofile.location} onChange={(e) => setUserprofile({ ...userprofile, location: e.target.value })} type="text" className="form-control"   />
               </div>
               
             </div>
             
             <div className="mt-5 text-center">
-              <button className="btn btn-primary profile-button" type="button">Save Profile</button>
+              <button onClick={handleProfileUpdate} className="btn btn-primary profile-button" type="button">Save Profile</button>
               
             </div>
           </div>
